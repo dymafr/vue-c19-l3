a5dc381e14497260557db371155285a5b69a9711
dymafr <erwan1309@hotmail.com>
2022-08-29 10:14:23 +0200

    1 file modified

--- src/shared/services/product.service.ts
+++ src/shared/services/product.service.ts
@@ -12,8 +12,10 @@ export async function fetchProducts(
     query.append('skip', (page - 1) * 20 + '');
   }
   query.append('limit', '20');
-  query.append('price', `$lte:${filter.priceRange[1]}`);
-  query.append('price', `$gte:${filter.priceRange[0]}`);
+  query.append(
+    'price',
+    `{"$gte":${filter.priceRange[0]}, "$lte":${filter.priceRange[1]}}`
+  );
   const products = await (
     await fetch(`https://restapi.fr/api/vueprojectproducts?${query}`)
   ).json();
